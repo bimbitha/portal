5d34b91027c57e50e5c41b637d942296907a0f34
TG016101 <thamme.gowda@cerner.com>
2019-06-07 14:14:40 +0530

    Dummy example with navigation

---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Practice makes man perfect.</p>
+        <ol>
+          <li>Bangalore</li>
+          <li>Tumkur</li>
+          <li>Kansas city</li>
+          <li>NewYouk</li>
+          <li>Singapore</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
